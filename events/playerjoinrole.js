11bf0fd1c742f52018f33e9cf2aaf59b8633bc1b
Darrion <52856180+Darrionat@users.noreply.github.com>
2020-08-23 17:13:51 -0400

    Added AutoRole

---
+++ events/playerjoinrole.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+// Plugins by Darrion Discord ID 601497373075570737
+const guildID = "601497373075570737";
+const memberRoleID = "601576493335969792";
+
+module.exports = {
+    name: "guildMemberAdd",
+
+    async execute(client, member) {
+        if (member.guild.id !== guildID) return;
+        if (member.user.bot) return;
+
+        let guild = client.bot.guilds.resolve(guildID);
+        var memberRole = guild.roles.cache.get(memberRoleID);
+        member.roles.add(memberRole);
+    }
+};
